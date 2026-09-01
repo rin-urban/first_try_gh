155e99471ade31c52c475f558a98b7c6d3e7fdde
rin-urban <rin-urban@yandex.ru>
2026-09-01 09:53:39 +0700

    part3 m3 version 2

--- module3_part3-task1.py
+++ module3_part3-task1.py
@@ -1,7 +1,9 @@
 #part3 - task1
 
-sides = input('Введите три стороны треугольника через пробел: ')
-a,b,c = sides.split()
-a,b,c = float(a), float(b), float(c)
+def triangle_square():
+	sides = input('Введите три стороны треугольника через пробел: ')
+	a,b,c = sides.split()
+	a,b,c = float(a), float(b), float(c)
+	return 'Площадь треугольника со сторонами ' + str(a) + ", " + str(b) + " и " + str(c) + " см равна " + str(round(((a+b+c)*(a+b)*(a+c)*(b+c))**0.5,1)) + " см2."
 
-print('Площадь трегуольника со сторонами ', a,", ", b," и ", c, " см равна ", round(((a+b+c)*(a+b)*(a+c)*(b+c))**0.5,2), "см2." )
+triangle_square()
